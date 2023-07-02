be72ccd841399d1e5fe992849bcb0f826ee3dd2e
dittamaz <129725385+dittamaz@users.noreply.github.com>
2023-07-02 17:27:47 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('demo......');
+import { server } from "./lib/server.js";
+
+
+export const initialFileStructure = () => {
+    console.log('Creating folders...');
+    console.log('Creating files...');
+}
+
+
+export const init = () => {
+    initialFileStructure();
+    server.init();
+}
+
+export const app = {
+    init,
+    initialFileStructure,
+}
+
+export default app;
+
+app.init();
---
+++ src/lib/server.ts
@@ -0,0 +1,10 @@
+export const init = () => {
+    console.log('Server init...')
+};
+import { IncomingMessage, ServerResponse } from "http";
+import http, { }
+export const server = {
+    init,
+}
+
+export default server;
